2b756e3df194cecc8c02bac99808ad5756125b73
Dua-Butt <bdua9833@gmail.com>
2025-09-17 12:28:42 +0500

    Update greeting message

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateBuilder(args); var app = builder.Build();
 
-app.MapGet("/", () => "Hello from simple .NET project with Nginx!");
+app.MapGet("/", () => "Hello from updated .NET project on port 8084!");
 
 app.Run();
